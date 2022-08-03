11a3a72a52ad50d12919c1ae006b51831b5e26ad
RoyaleHighrblx <96347364+RoyaleHighrblx@users.noreply.github.com>
2022-08-03 13:05:03 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 song_1 = "";
 song_2 = "";
 
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
 rightWristX = 0;
 rightWristY = 0;
 
@@ -32,6 +35,10 @@ function gotPoses(results){
     if(results.length > 0){
         console.log(results);
 
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
+
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
         console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
@@ -44,4 +51,28 @@ function gotPoses(results){
 
 function draw(){
     image(video, 0, 0, 300, 300);
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+if(scoreLefttWrist > 0.2){
+    circle(leftWristX,leftWristY,20);
+
+    stop();
+
+    if(song_1.stop() == false){
+        song_1.play();
+        song_1 = document.getElementById("song_name").innerHTML = "Harry Potter Theme Song";
+}
+}
+
+
+}
+
+function play(){
+song_variable.isPlaying();
+}
+
+function stop(){
+song_variable.stop();
 }
